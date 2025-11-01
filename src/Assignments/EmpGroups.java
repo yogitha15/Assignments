7067ed9419e20082d83006b48a1037136aa176b2
yogithamj <yogithamj1598@gmail.com>
2025-11-02 00:38:47 +0530

    Assignnment 3

--- src/Assignments/EmpGroups.java
+++ src/Assignments/EmpGroups.java
@@ -4,25 +4,23 @@ public class EmpGroups {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employees empNames= new Employees();
-		Employees empIds= new Employees();
-		
-		String empName []= new String[3];
-		
-		empName [0]=empNames.empName1;
-		empName [1]=empNames.empName2;
-		empName [2]=empNames.empName3;
-		
-		int[] empId=new int[3];
-		empId[0]=empIds.empId1;
-		empId[1]=empIds.empId2;
-		empId[2]=empIds.empId3;
-		
-		System.out.println ("Employee Name = "+ empName [0] +" , "+ "Employee Id = "+ empId[0]);
-		System.out.println ("Employee Name = "+ empName [1] +" , "+ "Employee Id = "+ empId[1]);
-		System.out.println ("Employee Name = "+ empName [2] +" , "+ "Employee Id = "+ empId[2]);
-		
-		
+		Employees Emp = new Employees();
+		//Employees empIds = new Employees();
+
+		String empName[] = new String[3];
+
+		empName[0] = Emp.empName1;
+		empName[1] = Emp.empName2;
+		empName[2] = Emp.empName3;
+
+		int[] empId = new int[3];
+		empId[0] = Emp.empId1;
+		empId[1] = Emp.empId2;
+		empId[2] = Emp.empId3;
+
+		System.out.println("Employee Name = " + empName[0] + " , " + "Employee Id = " + empId[0]);
+		System.out.println("Employee Name = " + empName[1] + " , " + "Employee Id = " + empId[1]);
+		System.out.println("Employee Name = " + empName[2] + " , " + "Employee Id = " + empId[2]);
 
 	}
 
